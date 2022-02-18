c47fd12fc0dab705a586d34d6a6b3102dfea8687
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-19 00:24:04 +0100

    async -get-response

--- weather.js
+++ weather.js
@@ -1,6 +1,26 @@
-const form =document.querySelector(".top banner form");
-const input =document.querySelector(".top banner input");
-const msg =document.querySelector(".top banner .msg");
+const form =document.querySelector(".top-banner form");
+console.log(form);
+const input =document.querySelector(".top-banner input");
+const msg =document.querySelector(".top-banner .msg");
 const list =document.querySelector(".ajax-section .cities");
 
 localStorage.setItem("apikey", EncryptStringAES("55a655968325e071837fdf958d641cd1"));
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    getWeatherDataFromApi();
+});
+
+
+const getWeatherDataFromApi =async()=>{
+    let apikey =DecryptStringAES(localStorage.getItem("apikey"));
+    console.log(apikey)
+    console.log(input.value);
+    let weatherType ="metric";
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apikey}`
+
+    const response = await axios.get(url);
+    console.log(response);
+    const { main,name,sys,weather} =response.data;
+
+}

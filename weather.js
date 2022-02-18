382e9b5944760d30d989c696863d9dcad282ccd0
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-18 23:12:24 +0100

    first commit

---
+++ weather.js
@@ -0,0 +1,6 @@
+const form =document.querySelector(".top banner form");
+const input =document.querySelector(".top banner input");
+const msg =document.querySelector(".top banner .msg");
+const list =document.querySelector(".ajax-section .cities");
+
+localStorage.setItem("apikey", EncryptStringAES("55a655968325e071837fdf958d641cd1"));
